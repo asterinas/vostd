cf324079fcc44a02ba431b7630f575dbcceae4a4
Xungan2 <2100012996@stu.pku.edu.cn>
2025-06-03 01:44:42 +0000

    Fix rw version

--- lock-protocol/src/spec/common.rs
+++ lock-protocol/src/spec/common.rs
@@ -1,6 +1,11 @@
+use std::path;
+
 use builtin::*;
 use builtin_macros::*;
-use vstd::prelude::*;
+use vstd::{prelude::*, seq::*};
+use vstd_extra::ghost_tree::Node;
+
+use crate::spec::utils::*;
 
 verus! {
 
@@ -12,170 +17,164 @@ pub open spec fn valid_cpu(cpu_num: CpuId, cpu: CpuId) -> bool {
     0 <= cpu < cpu_num
 }
 
-pub open spec fn valid_pos(size: nat, p: nat) -> bool {
-    0 <= p < size
-}
-
-pub open spec fn valid_range(size: nat, l: nat, r: nat) -> bool {
-    0 <= l < r <= size
-}
-
-pub open spec fn pos_in_range(l: nat, r: nat, p: nat) -> bool {
-    l <= p < r
-}
-
 pub open spec fn valid_pte_offset(offset: nat) -> bool {
     0 <= offset < 512
+}
+
+pub open spec fn wf_tree_path(path: Seq<NodeId>) -> bool {
+    if path.len() == 0 {
+        true
+    } else {
+        &&& path[0] == NodeHelper::root_id()
+        &&& forall|i: int|
+            1 <= i < path.len() ==> NodeHelper::is_child(path[i - 1], #[trigger] path[i])
+        &&& forall|i: int| 0 <= i < path.len() ==> #[trigger] NodeHelper::valid_nid(path[i])
+    }
 }
 
 } // verus!
 verus! {
 
 pub enum NodeState {
-    Empty,
-    EmptyLocked,
-    Free,
-    Locked,
-    LockedOutside,
+    UnAllocated,
+    WriteUnLocked,
+    WriteLocked,
 }
 
 pub enum CursorState {
-    Empty,
-    Creating(NodeId, NodeId, NodeId),
-    Hold(NodeId, NodeId),
-    Destroying(NodeId, NodeId, NodeId),
+    Void,
+    ReadLocking(Seq<NodeId>),
+    WriteLocked(Seq<NodeId>),
 }
 
 impl CursorState {
     pub open spec fn inv(&self) -> bool {
         match *self {
-            Self::Empty => true,
-            Self::Creating(st, en, cur_nid) => st < en && st <= cur_nid <= en,
-            Self::Hold(st, en) => st < en,
-            Self::Destroying(st, en, cur_nid) => st < en && st <= cur_nid <= en,
-        }
-    }
-
-    pub open spec fn get_locked_range(&self) -> (NodeId, NodeId) {
-        match *self {
-            CursorState::Empty => (arbitrary(), arbitrary()),
-            CursorState::Creating(st, _, en)
-            | CursorState::Hold(st, en)
-            | CursorState::Destroying(st, _, en) => (st, en),
-        }
-    }
-
-    pub open spec fn node_is_held(&self, nid: NodeId) -> bool {
-        match *self {
-            Self::Empty => false,
-            Self::Creating(st, en, cur_nid) => st <= nid < cur_nid,
-            Self::Hold(st, en) => st <= nid < en,
-            Self::Destroying(st, en, cur_nid) => st <= nid < cur_nid,
-        }
-    }
-
-    pub open spec fn node_is_not_held(&self, nid: NodeId) -> bool {
-        !self.node_is_held(nid)
-    }
-
-    pub open spec fn no_overlap(&self, cursor: &CursorState) -> bool {
-        match *self {
-            Self::Empty => true,
-            Self::Creating(st1, _, en1) | Self::Hold(st1, en1) | Self::Destroying(st1, _, en1) => {
-                match *cursor {
-                    Self::Empty => true,
-                    Self::Creating(st2, _, en2)
-                    | Self::Hold(st2, en2)
-                    | Self::Destroying(st2, _, en2) => st1 == en1 || st2 == en2 || en1 <= st2 || en2
-                        <= st1,
-                }
+            Self::Void => true,
+            Self::ReadLocking(path) => {
+                &&& 0 <= path.len() <= 3
+                &&& wf_tree_path(path)
+            },
+            Self::WriteLocked(path) => {
+                &&& 1 <= path.len() <= 4
+                &&& wf_tree_path(path)
             },
         }
     }
-}
 
-} // verus!
-verus! {
-
-pub enum SlotState {
-    Empty,
-    EmptyLocked,
-    Free,
-    Locked,
-    LockedOutside,
-}
-
-pub enum RangeState {
-    Empty,
-    Creating(nat, nat, nat),
-    Hold(nat, nat),
-    Destroying(nat, nat, nat),
-}
-
-impl RangeState {
-    pub open spec fn inv(&self) -> bool {
+    pub open spec fn get_path(&self) -> Seq<NodeId>
+        recommends
+            self.inv(),
+    {
         match *self {
-            Self::Empty => true,
-            Self::Creating(l, r, cur_p) => l < r && l <= cur_p <= r,
-            Self::Hold(l, r) => l < r,
-            Self::Destroying(l, r, cur_p) => l < r && l <= cur_p <= r,
+            Self::Void => Seq::empty(),
+            Self::ReadLocking(path) | Self::WriteLocked(path) => path,
         }
     }
 
-    pub open spec fn get_locked_range(&self) -> (nat, nat) {
+    pub open spec fn hold_write_lock(&self) -> bool
+        recommends
+            self.inv(),
+    {
         match *self {
-            Self::Empty => (arbitrary(), arbitrary()),
-            Self::Creating(l, _, r) | Self::Hold(l, r) | Self::Destroying(l, _, r) => (l, r),
+            Self::Void | Self::ReadLocking(_) => false,
+            Self::WriteLocked(_) => true,
         }
     }
 
-    pub open spec fn pos_is_held(&self, p: nat) -> bool {
+    pub open spec fn get_write_lock_node(&self) -> NodeId
+        recommends
+            self.inv(),
+            self.hold_write_lock(),
+    {
         match *self {
-            Self::Empty => false,
-            Self::Creating(l, r, cur_p) => l <= p < cur_p,
-            Self::Hold(l, r) => l <= p < r,
-            Self::Destroying(l, r, cur_p) => l <= p < cur_p,
+            Self::Void | Self::ReadLocking(_) => arbitrary(),
+            Self::WriteLocked(path) => path.last(),
         }
     }
 
-    pub open spec fn pos_is_not_held(&self, p: nat) -> bool {
-        !self.pos_is_held(p)
+    pub open spec fn get_read_lock_path(&self) -> Seq<NodeId>
+        recommends
+            self.inv(),
+    {
+        match *self {
+            Self::Void => Seq::empty(),
+            Self::ReadLocking(path) => path,
+            Self::WriteLocked(path) => path.drop_last(),
+        }
     }
 
-    pub open spec fn no_overlap(&self, range: &RangeState) -> bool {
-        match *self {
-            Self::Empty => true,
-            Self::Creating(l1, _, r1) | Self::Hold(l1, r1) | Self::Destroying(l1, _, r1) => {
-                match *range {
-                    Self::Empty => true,
-                    Self::Creating(l2, _, r2)
-                    | Self::Hold(l2, r2)
-                    | Self::Destroying(l2, _, r2) => l1 == r1 || l2 == r2 || r1 <= l2 || r2 <= l1,
-                }
-            },
-        }
+    pub open spec fn hold_read_lock(&self, nid: NodeId) -> bool
+        recommends
+            self.inv(),
+            NodeHelper::valid_nid(nid),
+    {
+        let path = self.get_read_lock_path();
+        path.contains(nid)
     }
 }
 
-} // verus!
-verus! {
-
-pub proof fn lemma_pos_in_range_set_finite(l: nat, r: nat)
+pub proof fn lemma_wf_tree_path_nid_increasing(path: Seq<NodeId>)
     requires
-        l <= r,
+        wf_tree_path(path),
     ensures
-        Set::new(|p: nat| pos_in_range(l, r, p)).finite(),
-        Set::new(|p: nat| pos_in_range(l, r, p)).len() == (r - l) as nat,
-    decreases r - l,
+        forall|i: int, j: int|
+            #![trigger path[i],path[j]]
+            0 <= i < j < path.len() ==> path[i] < path[j],
+    decreases path.len(),
 {
-    if l == r {
-        assert(Set::new(|p: nat| pos_in_range(l, r, p)) =~= Set::empty());
+    if path.len() == 0 {
+    } else if path.len() == 1 {
     } else {
-        lemma_pos_in_range_set_finite(l, (r - 1) as nat);
-        assert(Set::new(|p| pos_in_range(l, (r - 1) as nat, p)).insert((r - 1) as nat) =~= Set::new(
-            |p| pos_in_range(l, r, p),
-        ));
+        assert forall|i: int, j: int| 0 <= i < j < path.len() implies path[i] < path[j] by {
+            assert forall|i: int, j: int| 0 <= i < j < path.len() - 1 implies path[i] < path[j] by {
+                lemma_wf_tree_path_nid_increasing(path.drop_last());
+                assert(path[i] == path.drop_last()[i]);
+                assert(path[j] == path.drop_last()[j]);
+            }
+            admit();
+        }
     }
+}
+
+pub proof fn lemma_wf_tree_path_inversion(path: Seq<NodeId>)
+    requires
+        wf_tree_path(path),
+    ensures
+        path.len() > 0 ==> {
+            &&& path[0] == NodeHelper::root_id()
+            &&& wf_tree_path(path.drop_last())
+            &&& !path.drop_last().contains(path.last())
+        },
+{
+    if path.len() == 0 {
+    } else {
+        lemma_wf_tree_path_nid_increasing(path);
+    }
+}
+
+pub proof fn lemma_wf_tree_path_push_inversion(path: Seq<NodeId>, nid: NodeId)
+    requires
+        wf_tree_path(path.push(nid)),
+    ensures
+        wf_tree_path(path),
+        path.len() > 0 ==> NodeHelper::is_child(path.last(), nid),
+        !path.contains(nid),
+{
+    lemma_wf_tree_path_inversion(path.push(nid));
+    if (path.len() > 0) {
+        assert(path.push(nid).drop_last() =~= path);
+    }
+}
+
+pub enum AtomicCursorState {
+    Void,
+    Locked(NodeId),
+}
+
+pub enum NodeStability {
+    Stable,
+    Instable,
 }
 
 } // verus!
