14cbf2be87bb2e9f1056039db4f1f332b3bbb6f5
rikosellic <64517311+rikosellic@users.noreply.github.com>
2025-12-11 10:41:34 +0800

    Add special cases of deref_spec

--- vstd_extra/src/extern_func.rs
+++ vstd_extra/src/extern_func.rs
@@ -29,13 +29,35 @@ impl<T: Deref> DerefSpec for T {
 // Special Cases
 pub broadcast axiom fn ref_deref_spec<T>(r: &T)
     ensures
-        #[trigger] r.deref_spec() == r,
+        #[trigger] *(r.deref_spec()) == *r,
+;
+
+pub broadcast axiom fn box_deref_spec<T>(b: Box<T>)
+    ensures
+        #[trigger] *(b.deref_spec()) == *b,
+;
+
+pub broadcast axiom fn rc_deref_spec<T>(r: std::rc::Rc<T>)
+    ensures
+        #[trigger] *(r.deref_spec()) == *r,
+;
+
+pub broadcast axiom fn arc_deref_spec<T>(a: std::sync::Arc<T>)
+    ensures
+        #[trigger] *(a.deref_spec()) == *a,
 ;
 
 pub broadcast axiom fn manually_drop_deref_spec_eq<T: ?Sized>(v: &ManuallyDrop<T>)
     ensures
         #[trigger] &**v == manually_drop_deref_spec(v),
 ;
+
+pub broadcast group group_deref_spec {
+    ref_deref_spec,
+    box_deref_spec,
+    rc_deref_spec,
+    arc_deref_spec,
+}
 
 } // verus!
 verus! {
