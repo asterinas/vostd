e7843634fa8c1695b9d209f4117b8c0e72ccb960
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-01-15 14:12:16 +0800

    Use type invariant

--- vstd_extra/src/resource/storage/frac.rs
+++ vstd_extra/src/resource/storage/frac.rs
@@ -105,14 +105,14 @@ pub struct FracStorage<T> {
     r: Option<StorageResource<(), T, FracS<T>>>,
 }
 
-impl<T> Inv for FracStorage<T> {
-    closed spec fn inv(self) -> bool {
+impl<T> FracStorage<T> {
+    #[verifier::type_invariant]
+    spec fn type_inv(self) -> bool {
         &&& self.r is Some
         &&& self.protocol_monoid() is Frac
-        &&& 0.0real < self.frac() && self.frac() <= 1.0real}
+        &&& 0.0real < self.frac() && self.frac() <= 1.0real
     }
 
-impl<T> FracStorage<T> {
     pub closed spec fn storage_resource(self) -> StorageResource<(), T, FracS<T>> {
         self.r -> Some_0
     }
@@ -140,7 +140,6 @@ impl<T> FracStorage<T> {
     /// Allocate a new fractional storage resource with full permission.
     pub proof fn new(tracked v:T) -> (tracked res: Self)
     ensures
-        res.inv(),
         res.has_full_frac(),
     {
         let tracked mut m = Map::<(),T>::tracked_empty();
@@ -149,42 +148,54 @@ impl<T> FracStorage<T> {
         FracStorage { r: Some(resource) }
     }
 
-    /// Split the fractional storage resource into two fractions.
+    /// Split one token into two tokens whose quantities sum to the original.
     pub proof fn split(tracked &mut self) -> (tracked r: FracStorage<T>)
-    requires
-        old(self).inv(),
     ensures
-        self.inv(),
-        r.inv(),
+        self.id() == old(self).id(),
+        self.resource() == old(self).resource(),
+        r.resource() == old(self).resource(),
+        r.id() == old(self).id(),
         r.frac() + self.frac() == old(self).frac(),
     {
-        let frac = self.frac();
+        use_type_invariant(&*self);
+        Self::split_helper(&mut self.r)
+    }
+
+    /// Avoid breaking the type invariant.
+    proof fn split_helper(tracked r: &mut Option<StorageResource<(),T,FracS<T>>>) -> (tracked res: Self)
+    requires
+        *old(r) is Some,
+        old(r)->Some_0.value() is Frac,
+        old(r)->Some_0.value().frac() > 0.0real,
+        old(r)->Some_0.value().frac() <= 1.0real,
+    ensures
+        *r is Some,
+        r->Some_0.value() is Frac,
+        r->Some_0.value().frac() > 0.0real,
+        r->Some_0.value().frac() <= 1.0real,
+        res.id() == old(r)->Some_0.loc(),
+        r->Some_0.loc() == old(r)->Some_0.loc(),
+        r->Some_0.value().value() == old(r)->Some_0.value().value(),
+        res.resource() == old(r)->Some_0.value().value(),
+        res.frac() + r->Some_0.value().frac() == old(r)->Some_0.value().frac(),
+    {
+        let tracked mut storage_resource = r.tracked_take();
+        let frac = storage_resource.value().frac();
         let half_frac = frac / 2.0real;
-        let m = FracS::construct_frac(half_frac, self.resource());
-        let tracked resource = self.r.tracked_take();
-        let tracked (resource_1, resource_2) = resource.split(m,m);
-        self.r = Some(resource_1);
+        let m = FracS::construct_frac(half_frac, storage_resource.value().value());
+        let tracked (resource_1, resource_2) = storage_resource.split(m,m);
+        *r = Some(resource_1);
         FracStorage { r: Some(resource_2) }
     }
 
-    pub proof fn lemma_guards(self)
-    requires
-        self.inv(),
-    ensures
-        guards::<(),T,FracS<T>>(self.protocol_monoid(),map![() => self.resource()]),
-    {
-        self.protocol_monoid().lemma_guards();
-    }
-
-    /// Borrowing the ghost resource from the fractional reference.
+    /// Obtain shared access to the underlying resource.
     pub proof fn borrow(tracked &self) -> (tracked s: &T)
-    requires
-        self.inv(),
     ensures
         *s == self.resource(),
     {
+        use_type_invariant(self);
         let m = Map::<(),T>::empty().insert((), self.resource());
-        self.lemma_guards();
+        self.protocol_monoid().lemma_guards();
         let tracked resource = StorageResource::guard(self.r.tracked_borrow(), m);
         resource.tracked_borrow(())
     }
--- vstd_extra/src/resource/storage/mod.rs
+++ vstd_extra/src/resource/storage/mod.rs
@@ -2,10 +2,10 @@
 /// Product of a PCM and a storage-protocol resource algebra.
 pub mod hybrid_product;
 
-/// Fractional ownership.
-pub mod frac;
 /// A combination of fractional ownership and counter PCM.
 pub mod count_frac;
+/// Fractional ownership.
+pub mod frac;
 
-pub use hybrid_product::*;
 pub use frac::*;
+pub use hybrid_product::*;
