fd24847f4f75038facf9ab646d67d82f415d1292
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-03-18 14:26:21 +0800

    Add validate

--- vstd_extra/src/resource/ghost_resource/csum.rs
+++ vstd_extra/src/resource/ghost_resource/csum.rs
@@ -55,11 +55,16 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
     }
 
     #[verifier::type_invariant]
-    pub open spec fn type_invariant(self) -> bool {
+    pub open spec fn type_inv(self) -> bool {
         &&& TOTAL > 0
         &&& 1 <= self.frac() <= TOTAL
         &&& self.has_resource() || self.has_resource_taken()
         &&& self.is_left() || self.is_right()
+    }
+
+    #[verifier::inline]
+    pub open spec fn wf(self) -> bool {
+        self.type_inv()
     }
 
     pub proof fn alloc_left(tracked a: A) -> (tracked res: Self)
@@ -90,7 +95,14 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
         Self { r }
     }
 
-    pub proof fn lemma_resource_exlusive(tracked &mut self, tracked other: &Self)
+        pub proof fn validate(tracked &self)
+        ensures
+            self.wf()
+        {
+            use_type_invariant(&*self);
+        }
+
+    pub proof fn validate_with_other(tracked &mut self, tracked other: &Self)
         requires
             {
                 ||| old(self).is_left() && old(self).is_right()
@@ -107,7 +119,7 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
         }
     }
 
-    pub proof fn lemma_resource_exclusive_with_left(tracked &mut self, tracked other: &Left<A,B,TOTAL>)
+    pub proof fn validate_with_left(tracked &mut self, tracked other: &Left<A,B,TOTAL>)
     requires
         old(self).id() == other.id(),
     ensures
@@ -116,13 +128,14 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
         *old(self) == *self,
         self.is_left(),
         !(self.has_resource() && other.has_resource()),
+        self.frac() + other.frac() <= TOTAL,
     {
         use_type_invariant(&*self);
         use_type_invariant(other);
         self.r.validate_with_shared(&other.r);
     }
 
-    pub proof fn lemma_resource_exclusive_with_right(tracked &mut self, tracked other: &Right<A,B,TOTAL>)
+    pub proof fn validate_with_right(tracked &mut self, tracked other: &Right<A,B,TOTAL>)
     requires
         old(self).id() == other.id(),
     ensures
@@ -131,6 +144,7 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
         *old(self) == *self,
         self.is_right(),
         !(self.has_resource() && other.has_resource()),
+        self.frac() + other.frac() <= TOTAL,
     {
         use_type_invariant(&*self);
         use_type_invariant(other);
@@ -275,6 +289,10 @@ impl<A, B, const TOTAL:usize> SumResource<A, B, TOTAL> {
         (Self { r }, res)
     }
 
+    /* proof fn join_left(tracked self, tracked other: Left<A,B,TOTAL>) -> (tracked res: Self)
+        requires
+            self.id() == other.id(),
+        ensures */
 }
 
 impl<A, B, const TOTAL:usize> Left<A, B, TOTAL> {
@@ -291,9 +309,15 @@ impl<A, B, const TOTAL:usize> Left<A, B, TOTAL> {
     }
 
     #[verifier::type_invariant]
-    pub open spec fn type_invariant(self) -> bool {
+    pub open spec fn type_inv(self) -> bool {
         &&& self.protocol_monoid().is_left()
         &&& 1 <= self.frac() <= TOTAL
+        &&& self.has_resource() || self.has_resource_taken()
+    }
+
+    #[verifier::inline]
+    pub open spec fn wf(self) -> bool {
+        self.type_inv()
     }
 
     pub open spec fn has_resource(self) -> bool {
@@ -308,21 +332,27 @@ impl<A, B, const TOTAL:usize> Left<A, B, TOTAL> {
         self.protocol_monoid().frac()
     }
 
-    pub proof fn lemma_resource_exclusive(tracked &mut self, tracked other: &Self)
-        requires
-            old(self).has_resource() && other.has_resource()
-        ensures
-           *old(self) == *self,
-            self.id() != other.id(),
+    pub proof fn validate(tracked &self)
+    ensures
+        self.wf()
     {
         use_type_invariant(&*self);
-        use_type_invariant(other);
-        if self.id() == other.id() {
-            self.r.validate_with_shared(&other.r);
-        }
     }
 
-    pub proof fn lemma_resource_exclusive_with_right(tracked &self, tracked other: &Right<A,B,TOTAL>)
+    pub proof fn validate_with_left(tracked &mut self, tracked other: &Self)
+        requires
+            old(self).id() == other.id(),
+        ensures
+            *old(self) == *self,
+            !(self.has_resource() && other.has_resource()),
+            self.frac() + other.frac() <= TOTAL,
+        {
+            use_type_invariant(&*self);
+            use_type_invariant(other);
+            self.r.validate_with_shared(&other.r);
+        }
+
+    pub proof fn validate_with_right(tracked &self, tracked other: &Right<A,B,TOTAL>)
         ensures
             self.id() != other.id(),
     {
@@ -409,9 +439,15 @@ impl<A, B, const TOTAL:usize> Right<A, B, TOTAL> {
     }
 
     #[verifier::type_invariant]
-    pub open spec fn type_invariant(self) -> bool {
+    pub open spec fn type_inv(self) -> bool {
         &&& self.protocol_monoid().is_right()
         &&& 1 <= self.frac() <= TOTAL
+        &&& self.has_resource() || self.has_resource_taken()
+    }
+
+    #[verifier::inline]
+    pub open spec fn wf(self) -> bool {
+        self.type_inv()
     }
 
     pub open spec fn has_resource(self) -> bool {
@@ -426,21 +462,14 @@ impl<A, B, const TOTAL:usize> Right<A, B, TOTAL> {
         self.protocol_monoid().frac()
     }
 
-    pub proof fn lemma_resource_exclusive(tracked &mut self, tracked other: &Self)
-        requires
-            old(self).has_resource() && other.has_resource()
+    pub proof fn validate(tracked &self)
         ensures
-            *old(self) == *self,
-            self.id() != other.id(),
-    {
-        use_type_invariant(&*self);
-        use_type_invariant(other);
-        if self.id() == other.id() {
-            self.r.validate_with_shared(&other.r);
+            self.wf()
+        {
+            use_type_invariant(&*self);
         }
-    }
 
-    pub proof fn lemma_resource_exclusive_with_left(tracked &self, tracked other: &Left<A,B,TOTAL>)
+    pub proof fn validate_with_left(tracked &self, tracked other: &Left<A,B,TOTAL>)
         ensures
             self.id() != other.id(),
     {
@@ -451,6 +480,18 @@ impl<A, B, const TOTAL:usize> Right<A, B, TOTAL> {
         }
     }
 
+    pub proof fn validate_with_right(tracked &mut self, tracked other: &Self)
+        requires
+            old(self).id() == other.id(),
+        ensures
+            *old(self) == *self,
+            !(self.has_resource() && other.has_resource()),
+            self.frac() + other.frac() <= TOTAL,
+        {
+            use_type_invariant(&*self);
+            use_type_invariant(other);
+            self.r.validate_with_shared(&other.r);
+        }
 
     pub proof fn tracked_borrow(tracked &self) -> (tracked res: &B)
         requires
