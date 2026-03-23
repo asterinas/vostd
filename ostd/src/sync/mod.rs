bcfe1dae6f4ca149f2241af6d7f6ca0929ff9928
Marsman1996 <lqliuyuwei@outlook.com>
2026-03-23 10:47:53 +0800

    doc: enable for sync::mutex

--- ostd/src/sync/mod.rs
+++ ostd/src/sync/mod.rs
@@ -11,7 +11,7 @@ mod wait;
 //pub(crate) use self::rcu::finish_grace_period;
 pub use self::{
     guard::{GuardTransfer, LocalIrqDisabled, PreemptDisabled, SpinGuardian, /*WriteIrqDisabled*/},
-    //mutex::{ArcMutexGuard, Mutex, MutexGuard},
+    mutex::{ArcMutexGuard, Mutex, MutexGuard},
     rcu::{non_null /*, Rcu, RcuDrop, RcuOption, RcuOptionReadGuard, RcuReadGuard*/},
     rwarc::{RoArc, RwArc},
     rwlock::{RwLock, RwLockReadGuard, RwLockUpgradeableGuard, RwLockWriteGuard},
