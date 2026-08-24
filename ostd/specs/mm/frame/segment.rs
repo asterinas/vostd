8eb3f545fae4aaa43c8921191cc4192f10d72ff6
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-08-24 11:20:23 +0800

    fmt

--- ostd/specs/mm/frame/segment.rs
+++ ostd/specs/mm/frame/segment.rs
@@ -105,9 +105,7 @@ impl<M: AnyFrameMeta + ?Sized> Segment<M> {
                 &&& regions.frame_obligations.count(idx) >= 1
                 &&& regions.contains(idx)
                 &&& regions.slot_owners[idx].slot_vaddr == index_to_meta(idx)
-                &&& regions.slot_owners[idx].ref_count()
-                    > 0
-                &&& regions.slot_owners[idx].ref_count()
+                &&& 0 < regions.slot_owners[idx].ref_count()
                     <= crate::mm::frame::meta::REF_COUNT_MAX
                 &&& regions.slot_owners[idx].paths_in_pt.is_empty()
                 &&& regions.slot_owners[idx].usage is Frame
