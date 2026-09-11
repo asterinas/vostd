1309a157ebe5248ce646faa1a23fb96552dde769
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-09-11 15:13:54 +0800

    strengthen an unnecessarily weak precondition

--- ostd/src/util/ops.rs
+++ ostd/src/util/ops.rs
@@ -87,17 +87,12 @@ pub proof fn lemma_range_difference_set<T: FiniteRange + Ord>(a: Range<T>, b: Ra
 #[verus_spec(ret =>
     requires
         obeys_cmp::<T>(),
+        T::clone.requires((&a.start,)),
+        T::clone.requires((&a.end,)),
+        T::clone.requires((&b.start,)),
+        T::clone.requires((&b.end,)),
+        forall|x: T, cloned: T| #[trigger] T::clone.ensures((&x,), cloned) ==> cloned == x,
         finite_range_matches_ord::<T>(),
-        // Only the empty-b branch clones the endpoints.
-        !b.start.is_lt(&b.end) ==> {
-            &&& T::clone.requires((&a.start,))
-            &&& T::clone.requires((&a.end,))
-            &&& T::clone.requires((&b.start,))
-            &&& T::clone.requires((&b.end,))
-            &&& forall|x: T, cloned: T|
-                (x == a.start || x == a.end || x == b.start || x == b.end)
-                    && #[trigger] T::clone.ensures((&x,), cloned) ==> cloned == x
-        },
     ensures
         ret.obeys_prophetic_iter_laws() && ret.will_return_none() ==> {
             &&& ret.remaining() == range_difference_spec(*a, *b)
