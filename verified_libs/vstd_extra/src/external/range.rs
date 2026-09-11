143f5813837f66456c9f0a79844592c70da32339
Je5s1e <chaoccc22@gmail.com>
2026-09-11 14:02:43 +0800

    resolve confilct

--- verified_libs/vstd_extra/src/external/range.rs
+++ verified_libs/vstd_extra/src/external/range.rs
@@ -1,33 +1,11 @@
+use vstd::{
+    prelude::*,
+    std_specs::cmp::{PartialOrdIs, PartialOrdSpec},
+};
+
 use core::ops::{Range, RangeInclusive};
-use vstd::prelude::*;
 
 verus! {
-
-/// Length of a `Range<usize>`. Malformed ranges (`start > end`) are length 0,
-/// matching `ExactSizeIterator::len` for `Range<A: Step>` where `Step::steps_between`
-/// returns `None` on `end < start`, collapsed to 0.
-pub open spec fn range_usize_len_spec(r: &Range<usize>) -> usize {
-    if r.start < r.end {
-        (r.end - r.start) as usize
-    } else {
-        0usize
-    }
-}
-
-/// Exec-mode `len` for a `Range<usize>`: use in place of `r.len()` which is an
-/// `ExactSizeIterator` provided method and can't be specced with
-/// `assume_specification`.
-#[verifier::when_used_as_spec(range_usize_len_spec)]
-pub fn range_usize_len(r: &Range<usize>) -> (ret: usize)
-    ensures
-        ret == range_usize_len_spec(r),
-{
-    if r.start < r.end {
-        r.end - r.start
-    } else {
-        0
-    }
-}
 
 /// Whether a `Range<usize>` is empty. Malformed ranges (`start > end`) are empty.
 pub open spec fn range_usize_is_empty_spec(r: &Range<usize>) -> bool {
@@ -44,6 +22,13 @@ pub fn range_usize_is_empty(r: &Range<usize>) -> (ret: bool)
 {
     !(r.start < r.end)
 }
+
+/// See [`Range::is_empty`](https://doc.rust-lang.org/std/ops/struct.Range.html#method.is_empty).
+pub assume_specification<Idx: PartialOrd<Idx>>[ Range::<Idx>::is_empty ](r: &Range<Idx>) -> (res:
+    bool) where Idx: PartialOrd<Idx>
+    ensures
+        <Idx as PartialOrdSpec<Idx>>::obeys_partial_cmp_spec() ==> res == !r.start.is_lt(&r.end),
+;
 
 pub assume_specification<Idx>[ RangeInclusive::start ](r: &RangeInclusive<Idx>) -> (ret: &Idx)
     ensures
