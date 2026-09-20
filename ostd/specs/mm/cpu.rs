7cfbb575bf7ac631bcffe9e10af5a9d6ca2b1bc0
rikosellic <64517311+rikosellic@users.noreply.github.com>
2026-09-20 17:14:56 +0800

    fix: `CpuSet` import

--- ostd/specs/mm/cpu.rs
+++ ostd/specs/mm/cpu.rs
@@ -1,6 +1,16 @@
+use crate::cpu::CpuSet;
 use vstd::prelude::*;
 
 verus! {
+
+pub struct AtomicCpuSet;
+
+impl AtomicCpuSet {
+    #[verifier::external_body]
+    pub fn new(_initial: CpuSet) -> Self {
+        unimplemented!()
+    }
+}
 
 pub trait PinCurrentCpu {
 
